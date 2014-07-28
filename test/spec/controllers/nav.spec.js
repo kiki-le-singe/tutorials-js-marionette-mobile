a42b064cde204ad42ff25a531910b15bf2c6733d
Anthony Albertini <aalbertini@clever-age.com>
2014-07-28 17:39:48 +0200

    test(nav controller tests): Implement some tests for the nav controller

--- test/spec/controllers/nav.spec.js
+++ test/spec/controllers/nav.spec.js
@@ -1,8 +1,10 @@
 /* jshint expr: true*/
 define([
   'marionette',
-  'controllers/nav'
-], function (Marionette, NavController) {
+  'controllers/nav',
+  'models/movies',
+  'views/movielistitem'
+], function (Marionette, NavController, Movies, MovieListItemView) {
   'use strict';
 
   // unit test suite on nav controller
@@ -12,6 +14,39 @@ define([
 
     // hook running before each test
     beforeEach(function () {
+      var moviesMock = [
+        {
+          "__id": "nm0666739",
+          "i": [
+            "http://ia.media-imdb.com/images/M/MV5BMTY1OTY5NjI5NV5BMl5BanBnXkFtZTcwODA4MjM0OA@@._V1_.jpg",
+            681,
+            1024
+          ],
+          "l": "Aaron Taylor-Johnson",
+          "s": "Actor, Kick-Ass (2010)"
+        },
+        {
+          "__id": "nm1093951",
+          "i": [
+            "http://ia.media-imdb.com/images/M/MV5BMzE5MzI0MzY2OF5BMl5BanBnXkFtZTgwODE3MTk4MTE@._V1_.jpg",
+            1375,
+            1805
+          ],
+          "l": "Aaron Taylor-Johnson",
+          "s": "Actor, Kick-Ass (2010)"
+        },
+        {
+          "__id": "nm5025209",
+          "i": [
+            "http://ia.media-imdb.com/images/M/MV5BMTgwNzM0NDAxOV5BMl5BanBnXkFtZTcwMDYzMTc3Nw@@._V1_.jpg",
+            600,
+            850
+          ],
+          "l": "Aaron Taylor-Johnson",
+          "s": "Actor, Kick-Ass (2010)"
+        }
+      ];
+
       document.body.appendChild(element);
 
       var region = new Marionette.Region({
@@ -19,7 +54,8 @@ define([
       });
 
       this.controller = new NavController({
-        region: region
+        region: region,
+        movies: new Movies(moviesMock)
       });
     });
 
@@ -27,6 +63,7 @@ define([
     afterEach(function () {
       this.controller.destroy();
       this.controller = null;
+      element.innerHTML = '';
 
       document.body.removeChild(element);
     });
@@ -36,10 +73,17 @@ define([
       it('should render the movie list view', function () {
         // query the after
         var view = this.controller.list().currentView;
+        // console.log('this.controller.region', this.controller.region.$el.find('ul.table-view').length);
+
+        // view.$el.should.have.class('table-view');
       });
 
       it('should render the same number of items', function () {
+      });
 
+      it('should have three items', function () {
+        var view = this.controller.list().currentView;
+        view.collection.should.have.length(3);
       });
     });
 
